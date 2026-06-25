74ca6fc1c4fc0b43db8d65ff4ebf9104b21f568c
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-25 11:19:54 +0530

    added json input and output editor

--- components/JsonEditor.tsx
+++ components/JsonEditor.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Textarea } from './ui/textarea'
 
 const JsonEditor = () => {
   return (
-    <div>JsonEditor</div>
+    <Textarea 
+    placeholder="Paste your JSON here..."
+    className="w-full h-112.5 rounded=xl bg-slate-900 p-5 border border-slate-700 outline-none"
+    >
+
+    </Textarea>
   )
 }
 
--- components/OutputEditor.tsx
+++ components/OutputEditor.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Textarea } from './ui/textarea'
 
 const OutputEditor = () => {
   return (
-    <div>OutoutEditor</div>
+     <Textarea 
+     readOnly
+          placeholder="Formatted JSON..."
+
+    className="w-full h-112.5 rounded=xl bg-slate-900 p-5 border border-slate-700 outline-none"
+    >
+
+    </Textarea>
   )
 }
 
