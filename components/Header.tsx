00a04c1336f5c0b16b3c746c16c144042bcece61
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-25 11:12:05 +0530

    added header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,20 @@
+
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+   <header className="border-b border-slate-800">
+<div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between">
+      <h2 className="text-xl font-bold">
+          JSONCraft
+        </h2>
+          {/* todo mai theam dark and light */}
+        <button className="cursor-pointer">
+          🌙
+        </button>
+
+</div>
+   </header>
   )
 }
 
