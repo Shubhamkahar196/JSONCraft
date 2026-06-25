bcc513423ae1d49d434d7206d9994e1361471cc5
Shubham kahar <Shubhamkahar196@gmail.com>
2026-06-25 11:16:09 +0530

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,22 @@
+import { footer } from 'framer-motion/client'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="border-t border-slate-800 mt-50">
+        <div className="max-x-7xl mx-auto py-8 text-center">
+
+            <p>Made by Shubham kahar</p>
+            <p>shubhamkahar196@gmail.com</p>
+            <a
+          href="https://digitalheroesco.com"
+          target="_blank"
+          className="inline-block mt-4 bg-blue-600 px-5 py-2 rounded-lg"
+        >
+          Built for Digital Heroes
+        </a>
+        </div>
+    </footer>
   )
 }
 
